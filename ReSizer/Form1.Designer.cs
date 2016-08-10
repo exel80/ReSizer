0ccd7ebfc9485b5078ecd4028d194df65c424579
Exel80 <exel80p@gmail.com>
2016-08-10 20:23:06 +0300

    Add project files.

---
+++ ReSizer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ReSizer
+{
+    partial class ReSizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReSizer));
+            this.button1 = new System.Windows.Forms.Button();
+            this.program = new System.Windows.Forms.ComboBox();
+            this.size = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ReSize ME!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // program
+            // 
+            this.program.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.program.FormattingEnabled = true;
+            this.program.Location = new System.Drawing.Point(12, 12);
+            this.program.Name = "program";
+            this.program.Size = new System.Drawing.Size(229, 21);
+            this.program.TabIndex = 1;
+            // 
+            // size
+            // 
+            this.size.AccessibleDescription = "";
+            this.size.AccessibleName = "";
+            this.size.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.size.Items.AddRange(new object[] {
+            "640x360",
+            "720x405",
+            "848x480",
+            "960x540",
+            "1024x576",
+            "1280x720",
+            "1366x768",
+            "1600x900",
+            "1920x1080",
+            "2048x1152",
+            "2560x1152",
+            "2560x1440",
+            "2880x1620",
+            "3200x1800",
+            "3840x2160"});
+            this.size.Location = new System.Drawing.Point(12, 41);
+            this.size.Name = "size";
+            this.size.Size = new System.Drawing.Size(148, 21);
+            this.size.TabIndex = 2;
+            // 
+            // ReSizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 78);
+            this.Controls.Add(this.size);
+            this.Controls.Add(this.program);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ReSizer";
+            this.Text = "ReSizer 2.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox program;
+        private System.Windows.Forms.ComboBox size;
+    }
+}
+
---
+++ ReSizer/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ReSizer
+{
+    public partial class ReSizer : Form
+    {
+        // Define the FindWindow API function.
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+        // Define the SetWindowPos API function.
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, SetWindowPosFlags uFlags);
+        // Define the SetWindowPosFlags enumeration.
+        [Flags()]
+        private enum SetWindowPosFlags : uint
+        {
+            SynchronousWindowPosition = 0x4000,
+            DeferErase = 0x2000,
+            DrawFrame = 0x0020,
+            FrameChanged = 0x0020,
+            HideWindow = 0x0080,
+            DoNotActivate = 0x0010,
+            DoNotCopyBits = 0x0100,
+            IgnoreMove = 0x0002,
+            DoNotChangeOwnerZOrder = 0x0200,
+            DoNotRedraw = 0x0008,
+            DoNotReposition = 0x0200,
+            DoNotSendChangingEvent = 0x0400,
+            IgnoreResize = 0x0001,
+            IgnoreZOrder = 0x0004,
+            ShowWindow = 0x0040,
+        }
+
+        public ReSizer()
+        {
+            InitializeComponent();
+            ProcessToList();
+            program.SelectedIndex = 0;
+            size.SelectedIndex = 5;
+        }
+
+
+
+        private void ProcessToList()
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                if(process.MainWindowTitle != "")
+                    program.Items.Add(process.MainWindowTitle);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the target window's handle.
+            IntPtr target_hwnd = FindWindowByCaption(IntPtr.Zero, program.SelectedItem.ToString());
+            if (target_hwnd == IntPtr.Zero)
+            {
+                MessageBox.Show("Could not find a window with the title " + program.SelectedItem.ToString());
+                return;
+            }
+            // Set the window's position.
+            string[] WS = size.SelectedItem.ToString().Split('x');
+            int width = int.Parse(WS[0]) +4;
+            int height = int.Parse(WS[1]) +35;
+            int x = (1980 - width) /2;
+            int y = (1080 - height) /2;
+            SetWindowPos(target_hwnd, IntPtr.Zero, x, y, width, height, 0);
+        }
+    }
+}
